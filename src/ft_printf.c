f8fbe3e64d44dfb3ad55ee51cb73afff83f6d6ab
flauer <flauer@student.42heilbronn.de>
2023-09-28 12:14:02 +0200

    fix ft_fprintf

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flauer <flauer@student.42.fr>              +#+  +:+       +#+        */
+/*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 22:17:53 by flauer            #+#    #+#             */
-/*   Updated: 2023/06/01 14:59:14 by flauer           ###   ########.fr       */
+/*   Updated: 2023/09/28 12:11:44 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,34 @@ bool	handle_flag(char *curr_pos, t_printf *st)
 		ret &= f_putnbase(va_arg(st->args, unsigned int), UHEX, 1, &st->bytes);
 	else if (curr_pos[st->i] == '%')
 		ret &= f_putchar_fd('%', 1, &st->bytes);
+	else
+		return (false);
+	++(st->i);
+	return (ret);
+}
+
+bool	handle_flag_fd(char *curr_pos, t_printf *st, int fd)
+{
+	bool	ret;
+
+	++(st->i);
+	ret = true;
+	if (curr_pos[st->i] == 'c')
+		ret &= f_putchar_fd(va_arg(st->args, int), fd, &st->bytes);
+	else if (curr_pos[st->i] == 's')
+		ret &= f_putstr_fd(fd, va_arg(st->args, char *), &st->bytes);
+	else if (curr_pos[st->i] == 'p')
+		ret &= f_putptr(va_arg(st->args, void *), fd, &st->bytes);
+	else if (curr_pos[st->i] == 'd' || curr_pos[st->i] == 'i')
+		ret &= f_putnbr_fd(va_arg(st->args, int), fd, &st->bytes);
+	else if (curr_pos[st->i] == 'u')
+		ret &= f_putunbr(va_arg(st->args, unsigned int), fd, &st->bytes);
+	else if (curr_pos[st->i] == 'x')
+		ret &= f_putnbase(va_arg(st->args, unsigned int), LHEX, fd, &st->bytes);
+	else if (curr_pos[st->i] == 'X')
+		ret &= f_putnbase(va_arg(st->args, unsigned int), UHEX, fd, &st->bytes);
+	else if (curr_pos[st->i] == '%')
+		ret &= f_putchar_fd('%', fd, &st->bytes);
 	else
 		return (false);
 	++(st->i);
@@ -81,7 +109,8 @@ int	ft_fprintf(int fd, const char *format, ...)
 	{
 		if (format[st.i] == '%')
 		{
-			if (!f_putst(fd, format, &st) || !handle_flag((char *)format, &st))
+			if (!f_putst(fd, format, &st)
+				|| !handle_flag_fd((char *)format, &st, fd))
 				return (-1);
 			st.cws = st.i;
 		}
